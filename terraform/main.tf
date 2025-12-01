b4b24aeb269ffe494838efb97005495294be5a3a
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2025-12-01 10:19:34 -0600

    Fix formatting in Proxmox VM configuration: align indentation and spacing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,57 @@
 resource "proxmox_virtual_environment_vm" "test_vm" {
-    name      = "test-vm"
-    node_name = "zero"
-    started   = true
-    reboot    = true
-    on_boot   = false
-    machine   = "q35"
-    tags      = ["terraform", "ubuntu"]
-    bios      = "ovmf"
+  name      = "test-vm"
+  node_name = "zero"
+  started   = true
+  reboot    = true
+  on_boot   = false
+  machine   = "q35"
+  tags      = ["terraform", "ubuntu"]
+  bios      = "ovmf"
 
-    clone {
-        vm_id = 1000
-        full  = true
+  clone {
+    vm_id = 1000
+    full  = true
+  }
+
+  agent {
+    enabled = true
+  }
+
+  memory {
+    dedicated = 1024
+  }
+
+  cpu {
+    cores = 2
+    type  = "x86-64-v2-AES"
+  }
+
+  efi_disk {
+    datastore_id      = "local"
+    file_format       = "qcow2"
+    type              = "4m"
+    pre_enrolled_keys = true
+  }
+
+  initialization {
+    datastore_id = "vm-data"
+    file_format  = "qcow2"
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
     }
+  }
 
-    agent {
-        enabled = true
-    }
+  operating_system {
+    type = "l26"
+  }
 
-    memory {
-        dedicated = 1024
-    }
-
-    cpu {
-        cores = 2
-        type  = "x86-64-v2-AES"
-    }
-
-    efi_disk {
-      datastore_id = "local"
-      file_format  = "qcow2"
-      type         = "4m"
-      pre_enrolled_keys = true
-    }
-
-    initialization {
-        datastore_id = "vm-data"
-        file_format  = "qcow2"
-
-        ip_config {
-            ipv4 {
-                address = "dhcp"
-            }
-        }
-    }
-
-    operating_system {
-        type = "l26"
-    }
-
-    serial_device {}
+  serial_device {}
 
 }
 
 output "vm_ipv4_address" {
-    value = proxmox_virtual_environment_vm.test_vm.ipv4_addresses
+  value = proxmox_virtual_environment_vm.test_vm.ipv4_addresses
 }
