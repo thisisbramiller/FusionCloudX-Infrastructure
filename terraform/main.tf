2da85cc891dd2fb848ec4b2315e47b6b24075199
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2025-12-01 09:49:48 -0600

    Add initial Ansible and Terraform configuration files, including playbooks, roles, and variables

---
+++ terraform/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.88.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = "https://zero.fusioncloudx.home:8006/"
+  insecure = true
+
+  ssh {
+    agent    = true
+    username = "terraform"
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "test_vm" {
+    name      = "test-vm"
+    node_name = "zero"
+    started   = true
+    reboot    = true
+    on_boot   = false
+    machine   = "q35"
+    tags      = ["terraform", "ubuntu"]
+    bios = "ovmf"
+
+    clone {
+        vm_id = 1000
+        full  = true
+    }
+
+    agent {
+        enabled = true
+    }
+
+    memory {
+        dedicated = 1024
+    }
+
+    efi_disk {
+      datastore_id = "local"
+      file_format  = "qcow2"
+      type         = "4m"
+      pre_enrolled_keys = true
+    }
+
+    initialization {
+        datastore_id = "vm-data"
+        file_format  = "qcow2"
+
+        ip_config {
+            ipv4 {
+                address = "dhcp"
+            }
+        }
+    }
+
+    operating_system {
+        type = "l26"
+    }
+
+    serial_device {}
+
+}
+
+output "vm_ipv4_address" {
+    value = proxmox_virtual_environment_vm.test_vm.ipv4_addresses
+}
