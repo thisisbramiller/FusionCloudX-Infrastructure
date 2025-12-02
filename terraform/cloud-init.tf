478f8912cc9b1ff4cc8eb84880aeb42413861c0c
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2025-12-01 22:16:59 -0600

    Update hostname in cloud-init configuration to support dynamic naming

--- terraform/cloud-init.tf
+++ terraform/cloud-init.tf
@@ -6,23 +6,21 @@ resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
     source_raw {
         data = <<-EOF
         #cloud-config
-        hostname: test
+        hostname: test{{count.index + 1}}
         timezone: America/Chicago
         users:
-          - default
           - name: fcx
             groups:
               - sudo
             shell: /bin/bash
             sudo: ALL=(ALL) NOPASSWD:ALL
-        ssh_import_id: 
-          - gh:thisisbramiller
+            ssh_import_id: 
+              - gh:thisisbramiller
         package_update: true
         packages:
           - qemu-guest-agent
           - net-tools
           - curl
-          - ssh-import-id
         runcmd:
           - systemctl enable qemu-guest-agent
           - systemctl start qemu-guest-agent
