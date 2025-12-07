997f87834ebc5d2e5a2229c010fdce77274dc637
Branden Miller <76793954+thisisbramiller@users.noreply.github.com>
2025-12-07 10:30:44 -0600

    remove all test vms

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,44 +16,12 @@ variable "vm_configs" {
   }))
 
   default = {
-    "teleport" = {
-      vm_id      = 1101
-      name       = "teleport"
-      memory_mb  = 2048
-      cpu_cores  = 2
-      started    = true
-      full_clone = true
-    }
-    "semaphore" = {
+    "semaphore-ui" = {
       vm_id      = 1102
-      name       = "semaphore"
+      name       = "semaphore-ui"
       memory_mb  = 2048
       cpu_cores  = 2
-      started    = true
-      full_clone = true
-    }
-    "wazuh" = {
-      vm_id      = 1103
-      name       = "wazuh"
-      memory_mb  = 4096
-      cpu_cores  = 2
-      started    = true
-      full_clone = true
-    }
-    "immich" = {
-      vm_id      = 1104
-      name       = "immich"
-      memory_mb  = 4096
-      cpu_cores  = 2
-      started    = true
-      full_clone = true
-    }
-    "pi-hole" = {
-      vm_id      = 1105
-      name       = "pi-hole"
-      memory_mb  = 1024
-      cpu_cores  = 1
-      started    = true
+      started    = false
       full_clone = true
     }
   }
